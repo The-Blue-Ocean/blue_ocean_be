977f8124e794aa45963b7230aa943925d53aaaf3
paulybgood <paulybgood@gmail.com>
2021-12-21 11:59:05 -0500

    minor changes

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -3,9 +3,14 @@ const allRoutes = (app) => {
     const studentList = require('../controllers/studentControllers');
     const adminList = require('../controllers/adminControllers');
 
+<<<<<<< HEAD
 
     // Student routes
     // GET request for /api/students endpoint for all students
+=======
+    // Student routes
+    // GET request for /students endpoint for all students
+>>>>>>> e3395f34e8a2fff3204ebeb45d3581ffd4640efc
     app.route('/api/students').get(studentList.listAllStudents);
 
     // GET request for /api/students/:id endpoint for a student by their id
